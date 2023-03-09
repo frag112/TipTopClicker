29a03d7deccb6c86ac6b46a4a6e5a860d2c6bcb2
frag112 <lumchtime@gmail.com>
2023-03-09 10:46:55 +0300

    Updating store items
    начал делать скрипт по замене цвета кнопок и доступности, если у игрока достаточно денег на покупку. Сейчас цвет меняется но нужно еще альфу поправить

--- UI/UIHandler.cs
+++ UI/UIHandler.cs
@@ -6,6 +6,11 @@ using TMPro;
 
 public class UIHandler : MonoBehaviour
 {
+    [Header("Design Colors")]
+    [SerializeField] Color mainColor;
+    [SerializeField] Color activeColor;
+    [SerializeField] Color secondaryColor;
+    [SerializeField] Color backgroundColor;
     [Header("Top Panel")]
     [SerializeField] TextMeshProUGUI levelText;
     [SerializeField] TextMeshProUGUI storageText;
@@ -15,8 +20,8 @@ public class UIHandler : MonoBehaviour
     [SerializeField] TextMeshProUGUI currentAmountClicks;
     [SerializeField] TextMeshProUGUI clickButtonText;
     [Header("Store Panel")]
-    [SerializeField] TextMeshProUGUI upgradeClickText;
-    [SerializeField] TextMeshProUGUI upgradeClickCost;
+    [SerializeField] GameObject upgradeClick;
+    List<GameObject> ClickChildren = new List<GameObject>();
     [SerializeField] TextMeshProUGUI upgradeVaultText;
     [SerializeField] TextMeshProUGUI upgradeVaultCost;
     [SerializeField] TextMeshProUGUI upgradeSpeedText;
@@ -26,12 +31,14 @@ public class UIHandler : MonoBehaviour
     void OnEnable()
     {
         Counter.OnClicked += UpdateCountersTexts;
+        Counter.OnClicked += CheckStoreAvailability;
         Counter.OnLvlUp += UpdateLevelText;
         DataHandler.OnSave += ShowSaveLabel;
     }
     void OnDisable()
     {
         Counter.OnClicked -= UpdateCountersTexts;
+        Counter.OnClicked -= CheckStoreAvailability;
         Counter.OnLvlUp -= UpdateLevelText;
         DataHandler.OnSave -= ShowSaveLabel;
     }
@@ -39,6 +46,20 @@ public class UIHandler : MonoBehaviour
     {
         UpdateCountersTexts();
         UpdateLevelText();
+        CollectChildren(upgradeClick, ClickChildren);
+    }
+    void CollectChildren(GameObject father, List<GameObject> children)
+    {
+        var imageArray = father.GetComponentsInChildren<Image>();
+        foreach (var element in imageArray)
+        {
+            children.Add(element.gameObject);
+        }
+        var textArray = father.GetComponentsInChildren<TextMeshProUGUI>();
+        foreach (var element in textArray)
+        {
+            children.Add(element.gameObject);
+        }
     }
     void UpdateCountersTexts()
     {
@@ -48,6 +69,35 @@ public class UIHandler : MonoBehaviour
     void UpdateLevelText()
     {
         levelText.text = "LVL " + DataHandler.Instance.data.currentLvl;
+    }
+    void CheckStoreAvailability() // цвет меняется но альфа падает в ноль, нужно поправить ее тоже
+    {
+        if (DataHandler.Instance.data.currentAmoutCliks < 100)
+        {
+            foreach (var element in ClickChildren)
+            {
+                if (element.GetComponent<Image>() != null)
+                {
+                    element.GetComponent<Image>().color = mainColor;
+                }
+                if (element.GetComponent<TextMeshProUGUI>() != null)
+                {
+                    element.GetComponent<TextMeshProUGUI>().color = mainColor;
+                }
+            }
+        }else{
+                        foreach (var element in ClickChildren)
+            {
+                if (element.GetComponent<Image>() != null)
+                {
+                    element.GetComponent<Image>().color = secondaryColor;
+                }
+                if (element.GetComponent<TextMeshProUGUI>() != null)
+                {
+                    element.GetComponent<TextMeshProUGUI>().color = secondaryColor;
+                }
+            }
+        }
     }
     void ShowSaveLabel()
     {
